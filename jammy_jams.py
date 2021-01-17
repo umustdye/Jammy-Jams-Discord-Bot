d6d78c45fb4b91d5eae7960c56d74a81b37f0dcc
umustdye <heidibdye@gmail.com>
2021-01-16 17:19:52 -0800

    Can search and return link to Youtube video

--- jammy_jams.py
+++ jammy_jams.py
@@ -16,54 +16,62 @@ import nest_asyncio
 #initialize the client/bot
 #client = discord.Client()
 client = commands.Bot(command_prefix="!")
+BOT_TOKEN = ""
 
 
-"""
-#TEST THE YOUTUBE SEARCH--------------
-search = "Funny cats"
-results = YoutubeSearch(search, max_results=1).to_dict()
-#'url_suffix' is the /watch part of the youtube link
-print(results)
-"""
-
-
-"""
-#sample hello world
-@client.event
-#async make sures the process happens even if there is a delay in the process
-#on_ready is a prebuilt discord function
-async def on_ready():
-    #connect to the general text channel
-    general_channel = client.get_channel(799871091404570657)
-    #await waits for the channel to be found before preforming an action
-    await general_channel.send("Hello world")
-"""
 
 #pop-up message for when the bot enters the voice chat
 @client.event
 async def on_ready():
-        #general text channel
-        channel = client.get_channel(799871091404570657)
-        await channel.send("Jammy Jams is reporting for duty!")
+        #jammy jams messages channel
+        jammy_channel = client.get_channel(800145177690898473)
+        await jammy_channel.send("Jammy Jams is reporting for duty!")
 
 @client.command()
 async def join(ctx):
         #channel = discord.utils.get(ctx.guild.voice_channels, name="General")
         #general voice channel
         channel = client.get_channel(799871091404570658)
+        jammy_channel = client.get_channel(800145177690898473)
         await channel.connect()
+        await jammy_channel.send("Jammy Jams has entered the party.")
+        
+        
+@client.command()
+async def play(ctx, *search_terms):
+    #connect to the jammy messages channel
+    jammy_channel = client.get_channel(800145177690898473)
+    
+    
+    #put search terms into one string
+    search_term = ""
+    for term in search_terms:
+        search_term += term + " "
+    
+    #dictionary to hold the search term result from YouTube
+    results = YoutubeSearch(search_term, max_results=5).to_dict()
+    #if a result was found
+    if(results):
+        link = 'https://www.youtube.com' + results[0].get('url_suffix')
+        await jammy_channel.send(link)
+        
+    else:
+        await jammy_channel.send("Sorry Jammy could not find a cool enough video. Please make your request more specific and try again.")
+        
+
         
 @client.command()
 async def leave(ctx):
         #channel = discord.utils.get(client.voice_clients, guild=ctx.guild)
         channels = client.voice_clients
+        jammy_channel = client.get_channel(800145177690898473)
         for channel in channels:
             if channel.is_connected():
                 await channel.disconnect()
-            else:
-                await ctx.send("The bot is not connected to the voice channel")
+                await jammy_channel.send("Jammy Jams has left the music party.")
+
 
 #allow nested event loops to run the client/bot in the console
 nest_asyncio.apply() 
 #run the client/bot(bot token)
-client.run("Nzk5ODY4NTI4NDk1ODIwODUw.YAJ1ng.O_pY0IUfXoAXqim3NOZ2F7auutQ")
+client.run(BOT_TOKEN)
