6ffa497ed4b6691887a16753b8deed46f05cba95
umustdye <heidibdye@gmail.com>
2021-01-30 17:46:12 -0800

    added play_queue command to play from the beginning of the queue

--- jammy_jams.py
+++ jammy_jams.py
@@ -120,9 +120,7 @@ async def play_next_song(ctx):
     time.sleep(.5)
     
     await text_channel.send(song_queue[0]['video_name'] + " has finished playing.")
-    print("Hello")
-    print(song_queue)
-    print(if_play_queue)
+
     
     #if the user did not want to repeat the current song then move to the next song in the queue
     if repeat_song == False and if_play_queue == False:
@@ -463,130 +461,6 @@ async def repeat(ctx):
 
 
 
-
-# FOR FUN RANDOM COMMANDS!-----------------------------------------------------------
-
-@client.command(pass_context = True)
-async def love(ctx, *, person):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("I love you "+ person)
-  
-@client.command(pass_context = True)
-async def love_response(ctx, *, person):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("I love you more " + person)      
- 
-@client.command(pass_context = True)    
-async def kiss(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("A Heidi kiss for you")
-    
-    
-@client.command(pass_context = True)    
-async def fat(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Wooooooooowwww I am not fat")
-    
-    
-@client.command(pass_context = True)    
-async def bigbutt(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    #connect to the jammy voice channel
-    voice_channel = client.get_channel(voice_channel_id)
-    if (connected(ctx)==None):
-        await voice_channel.connect()
-    link = 'https://www.youtube.com/watch?v=X53ZSxkQ3Ho'
-    video_name = "Baby Got Back"
-    file_name = "..\\pictures\\butt.jpg"
-    with open(file_name, 'rb') as f:
-        await text_channel.send("A Big Butt Heidi for my Honey Bunny")
-        await text_channel.send(file=File(f, 'big_butt_heidi.jpg'))
-        
-    global song_queue
-    next_song = {'video_name':video_name, 'link':link}
-    song_queue.append(next_song)    
-
-    play_song(ctx, link, video_name)
-
-
-@client.command(pass_context = True)    
-async def tease(ctx, *, message):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Wooooooooowwww I do not have a "+ message + "\nMeanie") 
-    
-    
-@client.command(pass_context = True)
-async def witch(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    file_name = "..\pictures\witchy_heidi.jpg"
-    with open(file_name, 'rb') as f:
-        await text_channel.send("A Witchy Heidi for my Honey Bunny")
-        await text_channel.send(file=File(f, 'witchy_heidi.jpg'))
-        
-@client.command(pass_context = True)
-async def cuteheidiface(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    pic_indx = random.randint(1, 11)
-    file_name = "..\pictures\cute_heidi\\" + str(pic_indx) + ".jpg"
-    with open(file_name, 'rb') as f:
-        await text_channel.send("A Cute Faced Heidi for my Honey Bunny")
-        await text_channel.send(file=File(f, 'cute_heidi.png'))   
-        
-        
-@client.command(pass_context = True)
-async def boobp(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    file_name = "..\pictures\\boobp.jpg"
-    with open(file_name, 'rb') as f:
-        await text_channel.send("Boobp")
-        await text_channel.send(file=File(f, 'boobp.jpg'))  
-
-
-@client.command(pass_context = True)
-async def sexyheidi(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    pic_indx = random.randint(1, 9)
-    file_name = "..\pictures\sexy_heidi\\" + str(pic_indx) + ".jpg"
-    with open(file_name, 'rb') as f:
-        await text_channel.send("A Sexy Heidi for my Honey Bunny")
-        await text_channel.send(file=File(f, 'sexy_heidi.png'))   
-
-
-@client.command(pass_context = True)
-async def sebastian(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Sebastian is Heidi's one and only love in life and she loves him with all her heart")
-        
-@client.command(pass_context = True)
-async def horndog(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Oh my...")
-
-@client.command(pass_context = True)
-async def butt(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Wowwwwwwwwww")      
-        
-@client.command(pass_context = True)
-async def pew(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Pew")
-    
-@client.command(pass_context = True)
-async def poke(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Poke")
-
-
-@client.command(pass_context = True)
-async def boop(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Boop")
-    
-@client.command(pass_context = True)
-async def meow(ctx):
-    text_channel = client.get_channel(text_channel_id)
-    await text_channel.send("Meow")
 
 #allow nested event loops to run the client/bot in the console
 nest_asyncio.apply() 
